0f853da4acab7453964ac46e00d584543c6d1a53
chenyi-w <70153414+chenyi-w@users.noreply.github.com>
2022-09-01 12:00:42 -0400

    Update ball.js

--- examples/ball.js
+++ examples/ball.js
@@ -5,11 +5,15 @@ class Ball {
     this.size = 20;
     this.xDir = random(1, 3);
     this.yDir = random(1, 3);
+    //random color
+    this.r = random(255);
+    this.g = random(255);
+    this.b = random(255);
   }
   
   draw() {
     noStroke();
-    fill(255, 0, 0);
+    fill(this.r, this.g, this.b);  // random color
     ellipse(this.x, this.y, this.size, this.size);
   }
   
@@ -18,9 +22,17 @@ class Ball {
     this.y += this.yDir;
     if (this.x >= (width - 10) || this.x < 10) {
       this.xDir = -this.xDir; 
+      //change color when hit the boundaries
+      this.r = random(255);
+      this.g = random(255);
+      this.b = random(255);
     }
     if (this.y >= (height - 10) || this.y < 10) {
       this.yDir = -this.yDir; 
+      //change color when hit the boundaries
+      this.r = random(255);
+      this.g = random(255);
+      this.b = random(255);
     }
   }
 }
@@ -39,7 +51,8 @@ function setup() {
 function draw() {
   background(220);
   
-  if (frameCount > 240) frameRate(10); //slow down
+  //slow down
+  //if (frameCount > 240) frameRate(10); 
   
   for (var i = 0; i < ball.length; i++){     
     ball[i].draw();
