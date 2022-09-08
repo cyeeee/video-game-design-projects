0bd1a82b305e497add9c7c8d8c9f031baacb57b4
Chenyi Wang <chenyiw@vt.edu>
2022-09-08 16:57:59 -0400

    second commit

--- Project1/sketch.js
+++ Project1/sketch.js
@@ -37,27 +37,80 @@ class Ball {
   }
 }
 
+class Invader {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  
+  draw() {
+    fill(0, 255, 255);
+    noStroke();
+    rect(this.x, this.y, 15, 7);
+    rect(this.x-3, this.y+2, 21, 2);
+    rect(this.x+3, this.y-3, 9, 3);
+    rect(this.x+3, this.y+7, 2, 3);
+    rect(this.x+10, this.y+7, 2, 3);
+    quad(this.x+1, this.y-6, this.x+3, this.y-6, 
+      this.x+6, this.y-3, this.x+4, this.y-3);
+    quad(this.x+12, this.y-6, this.x+14, this.y-6, 
+      this.x+11, this.y-3, this.x+9, this.y-3);
+    fill(0);
+    square(this.x+3, this.y+1, 2);
+    square(this.x+10, this.y+1, 2);
+  }
+}
+
 class Gun {
   constructor(x) {
     this.x = x;
   }
 
   draw() {
-    fill(0);
-    
+    fill(109, 113, 46); // green
+    noStroke();
+    rect(this.x, 395, 40, 5);
+    rect(this.x+7, 385, 26, 10);
+    rect(this.x+17, 380, 6, 5);
+  }
+
+  move() {
+    if (keyArray[LEFT_ARROW] === 1 && this.x >= 0) {
+      this.x--;
+    }
+    if (keyArray[RIGHT_ARROW] === 1 && this.x <= (width-40)) {
+      this.x++;
+    }
   }
 }
 
 var ball;
+var gun;
+var invader;
+var keyArray = [];
+
+function keyPressed() {
+  keyArray[keyCode] = 1;
+}
+function keyReleased() {
+  keyArray[keyCode] = 0;
+}
 
 function setup() {
   createCanvas(400, 400);
-  ball = new Ball(200, 200);   
+  ball = new Ball(200, 200);
+  gun = new Gun(180);
+  invader = new Invader(200, 200);
 }
 
 function draw() {
-  background(255);
+  background(0);
 
   ball.draw();
   ball.move();
+
+  invader.draw();
+
+  gun.draw();
+  gun.move();
 }
