3258b2d61786aba66e621cc16dabb7e83112f37f
Chenyi Wang <chenyiw@vt.edu>
2022-09-14 20:33:23 -0400

    update

--- Project2/sketch.js
+++ Project2/sketch.js
@@ -1,12 +1,44 @@
+/*
+var tilemap = [
+  "                                        ",
+  "wwwwwwwwwwwww       ",
+  "w           w       ",
+  "w           w       ",
+  "w           www     ",
+  "w                  w",
+  "w                  w",
+  "w                  w",
+  "wwwwwwwwwwww       w",
+  "w                  w",
+  "w               c  w",
+  "w                  w",
+  "w                  w",
+  "w                  w",
+  "w                 ww",
+  "w                www",
+  "w               wwww",
+  "w             wwwwww",
+  "w        wwwwwwwwwww",
+  "gggggggggggggggggggg",];
+  */
 
 var objects = [];
 
 function customChar() {
 
+  // main character
   fill(255, 0);
   rect(0, 0, 400, 400);
   //temp
   fill(0);
+  circle(200, 200, 300);
+  objects.push(get(0, 0, width, height));
+
+  // enemy
+  fill(255, 0);
+  rect(0, 0, 400, 400);
+  //temp
+  fill(255, 0, 0);
   circle(200, 200, 300);
   objects.push(get(0, 0, width, height));
 
@@ -53,12 +85,43 @@ class mainCharObj {
   }
 }
 
+class enemyObj {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.xDir = random(-0.5, 0.5);
+    this.yDir = random(-0.5, 0.5);
+  }
+
+  draw() {
+    //todo
+    image(objects[1], this.x, this.y, 20, 20);
+  }
+
+  move() {
+    // the enemies wander around
+    this.x += this.xDir;
+    this.y += this.yDir;
+    // stay inside the tilemap
+    if (this.x >= (800 - 10) || this.x < 10) {
+      this.xDir = -this.xDir; 
+    }
+    if (this.y >= (800 - 10) || this.y < 10) {
+      this.yDir = -this.yDir; 
+    }
+    // when enemies see the main character, they will chase the main character
+
+  }
+}
+
 var mainChar;
+var enemies;
 
 function setup() {
   createCanvas(400, 400);
   customChar();
   mainChar = new mainCharObj(100, 100);
+  enemies = new enemyObj(200, 200);
 }
 
 function draw() {
@@ -67,4 +130,6 @@ function draw() {
   mainChar.draw();
   mainChar.move();
   
+  enemies.draw();
+  enemies.move();
 }
