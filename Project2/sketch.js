5d71c14a93e20b26a9aedc7fe80f6e50ada00b20
Chenyi Wang <chenyiw@vt.edu>
2022-09-16 17:31:38 -0400

    Implemented proj2

--- Project2/sketch.js
+++ Project2/sketch.js
@@ -128,26 +128,46 @@ class mainCharObj {
       this.y += 2;
     }
     // stay inside the tilemap
-    if (this.x < 0) {
-      this.x = 0;
+    if (this.x < 20) {
+      this.x = 20;
     }
     else if (this.x > 800) {
       this.x = 800;
     }
-    else if (this.y < 0) {
-      this.y = 0;
+    else if (this.y < 20) {
+      this.y = 20;
     }
     else if (this.y > 800) {
       this.y = 800;
     }
     // the prizes are collected when the main character touches it
     for (var i = 0; i < game.prizes.length; i++) {
-      if (dist(this.x, this.y, game.prizes[i].x, game.prizes[i].y) < 15) {
+      if (game.prizes[i].collected === false && dist(this.x, this.y, game.prizes[i].x, game.prizes[i].y) < 15) {
         game.prizes[i].collected = true;
         game.score++;
       }
     }
     // bounce back a little when bumping into a rock
+    for (var i = 0; i < game.rocks.length; i++) {
+      if (dist(this.x, this.y, game.rocks[i].x, game.rocks[i].y) < 20) {
+        if (this.x < game.rocks[i].x) {
+          this.x -= 5;
+          game.xCor += 5;
+        }
+        if (this.x > game.rocks[i].x) {
+          this.x += 5;
+          game.xCor -= 5;
+        }
+        if (this.y < game.rocks[i].y) {
+          this.y -= 5;
+          game.yCor += 5;
+        }
+        if (this.y > game.rocks[i].y) {
+          this.y -= 5;
+          game.yCor += 5;
+        }
+      }
+    }
   }
 }
 
@@ -157,30 +177,54 @@ class enemyObj {
     this.y = y;
     this.xDir = random(-0.2, 0.2);
     this.yDir = random(-0.2, 0.2);
+    this.step = new p5.Vector(0, -1);
+    this.angle = 0;
   }
 
   draw() {
     image(objects[3], this.x, this.y, 20, 20);
   }
 
-  /*
   move() {
     // the enemies wander around
     this.x += this.xDir;
     this.y += this.yDir;
     // stay inside the tilemap
-    if (this.x >= (800 - 10) || this.x < 10) {
+    if (this.x >= 800 || this.x < 20) {
       this.xDir = -this.xDir; 
     }
-    if (this.y >= (800 - 10) || this.y < 10) {
+    if (this.y >= 800 || this.y < 20) {
       this.yDir = -this.yDir; 
     }
-    // when enemies see the main character, they will chase the main character
-
     // bounce back a little when bumping into a rock
-
+    for (var i = 0; i < game.rocks.length; i++) {
+      if (dist(this.x, this.y, game.rocks[i].x, game.rocks[i].y) < 20) {
+        if (this.x < game.rocks[i].x) {
+          this.x -= 5;
+        }
+        if (this.x > game.rocks[i].x) {
+          this.x += 5;
+        }
+        if (this.y < game.rocks[i].y) {
+          this.y -= 5;
+        }
+        if (this.y > game.rocks[i].y) {
+          this.y -= 5;
+        }
+      }
+    }
+    // when enemies see the main character, they will chase the main character
+    if (dist(this.x, this.y, mainChar.x, mainChar.y) < 60) {
+      this.step.set(mainChar.x - this.x, mainChar.y - this.y);
+      this.step.normalize();
+      this.angle = this.step.heading() + HALF_PI;
+      this.x += this.step.x;
+      this.y += this.step.y;
+      if (dist(this.x, this.y, mainChar.x, mainChar.y) < 10) {
+        game.gameOver = true;
+      }
+    }
   }
-  */
 }
 
 class gameObj {
@@ -193,12 +237,12 @@ class gameObj {
       "b                                        b",
       "b          r                p            b",
       "b   p                                    b",
-      "b        e                               b",
+      "b                                        b",
       "b                      p          r      b",
-      "b      r                      e          b",
+      "b      r                                 b",
       "b                                        b",
       "b  p                                     b",
-      "b                e               r       b",
+      "b                                r       b",
       "b                         p              b",
       "b                                        b",
       "b         p                       p      b",
@@ -211,9 +255,9 @@ class gameObj {
       "b                                  p     b",
       "b                                        b",
       "b                  r                     b",
-      "b   p                           e        b",
+      "b   p                                    b",
       "b                                        b",
-      "b        e                               b",
+      "b                                        b",
       "b                                        b",
       "b                   p          p         b",
       "b                                        b",
@@ -221,19 +265,16 @@ class gameObj {
       "b   r     p                              b",
       "b                             p          b",
       "b                                        b",
-      "b           e   p                        b",
+      "b               p                        b",
       "b                                        b",
       "b                            p           b",
       "b                                        b",
       "b       p            r                   b",
       "b                                        b",
       "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",];
-    //todo
     this.prizes = [];
     this.rocks = [];
-    this.enemies = [];
     this.score = 0;
-    this.win = false;
     this.gameOver = false;
     this.xCor = -200;
     this.yCor = -200;
@@ -246,8 +287,6 @@ class gameObj {
           case 'p': this.prizes.push(new prizeObj(j*20, i*20));
           break;
           case 'r': this.rocks.push(new rockObj(j*20, i*20));
-          break;
-          case 'e': this.enemies.push(new enemyObj(j*20, i*20));
           break;
         }
       }
@@ -262,62 +301,56 @@ class gameObj {
           break;
           case 'r': image(objects[1], j*20, i*20, 20, 20);
           break;
-          case 'e': image(objects[3], j*20, i*20, 20, 20);
-          break;
           case 'b': image(objects[4], j*20, i*20, 20, 20);
           break;
         }
       }
     }
-
-    /* if (this.xCor >= -300 && this.xCor <= 500) {
-      this.xCor -= mainChar.x - 400;
-      mainChar.x = 400;
-    } */
-    //if (this.xCor <= 0 && this.yCor <= 0) {
-      if (keyIsDown(LEFT_ARROW)) {
-        this.xCor += 2;
-      }
-      if (keyIsDown(RIGHT_ARROW)) {
-        this.xCor -= 2;
-      }
-      if (keyIsDown(UP_ARROW)) {
-        this.yCor += 2;
-      }
-      if (keyIsDown(DOWN_ARROW)) {
-        this.yCor -= 2;
-      }
-    //}
-
-
+    // scroll the map as the main character moves around
+    if (keyIsDown(LEFT_ARROW)) {
+      this.xCor += 2;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      this.xCor -= 2;
+    }
+    if (keyIsDown(UP_ARROW)) {
+      this.yCor += 2;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      this.yCor -= 2;
+    }
   }
 }
 
 var mainChar;
-//var enemies = [];
+var enemies = [];
 var game;
 
 function setup() {
   createCanvas(400, 400);
   customChar();
   mainChar = new mainCharObj(400, 400);
-  /*
   enemies[0] = new enemyObj(480, 300);
   enemies[1] = new enemyObj(680, 420);
   enemies[2] = new enemyObj(360, 500);
   enemies[3] = new enemyObj(460, 720);
   enemies[4] = new enemyObj(460, 120);
   enemies[5] = new enemyObj(80, 260);
-  */
   game = new gameObj();
   game.initialize();
 }
 
 function draw() {
-
-  if (game.gameOver === false) {
+  if (game.score === 20) {  // collected all 20 of prizes
     background(200, 255, 200);  // light green
+    fill(0);
+    textStyle(BOLD);
+    textFont('Courier New', 40);
+    text("YOU WIN!", 100, 200);
+  }
 
+  else if (game.gameOver === false) {
+    background(200, 255, 200);  // light green
     push();
     translate(game.xCor, game.yCor);
     game.drawBackground();
@@ -328,27 +361,19 @@ function draw() {
     }
     mainChar.draw();
     mainChar.move();
-    pop();
-
-    //game.initialize();
-    //game.drawBackground(); 
-
-    /* for (var i = 0; i < game.prizes.length; i++) {
-      if (game.prizes[i].collected === true) {
-        game.prizes[i].remove();
-      }
-    } */
-  
-    //mainChar.draw();
-    //mainChar.move();
-  
-    /*
     for (var i = 0; i < enemies.length; i++) {
       enemies[i].draw();
-      //enemies[i].move();
+      enemies[i].move();
     }
-    */
+    pop();
   }
-  
+
+  else {  // game over
+    background(200, 255, 200);
+    fill(0);
+    textStyle(BOLD);
+    textFont('Courier New', 40);
+    text("GAME OVER", 80, 200);
+  }
   
 }
